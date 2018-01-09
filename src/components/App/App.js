2e2d8c68fdf6a3b4fc4788a228e7775ffd1fdcf9
Dean Hamann <hamann.dean@gmail.com>
2018-01-08 19:35:51 -0800

    Integrated OpenWeatherMap API

---
+++ src/components/App/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import API from '../../api/api'; 
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  };
+
+  componentDidMount(){
+    API.fetchWeatherForcast().then((data) => console.log(data)); 
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+};
+
+export default App;
