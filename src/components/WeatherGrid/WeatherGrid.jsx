42968be0958271933c40e9ce2f7b71b6035823ea
Dean Hamann <hamann.dean@gmail.com>
2018-01-11 00:56:38 -0800

    Constructed Weather Table

--- src/components/WeatherGrid/WeatherGrid.jsx
+++ src/components/WeatherGrid/WeatherGrid.jsx
@@ -6,20 +6,32 @@ const WeatherGrid = ({weather}) => {
 	return (
     <div>
       {!weather ? <p>Loading</p> :  
-      <div>
-          <h2> Weather in {weather.city.name}</h2>
-            <h3> {weather.list[0].main.temp} ° Degrees </h3> 
-            <table>
-              <tbody>
-                <tr>
-                  <td>Wind</td>
-                  <td>{weather.list[0].wind.speed}</td> 
-                </tr>
-                <tr></tr>
-                <tr></tr>
-                <tr></tr>
-                <tr></tr>
-                <tr></tr>
+      <div className= "weatherTable">
+        <h2> Weather in {weather.city.name}</h2>
+        <h3> {weather.list[0].main.temp} ° Degrees </h3> 
+          <table>
+            <tbody>
+              <tr>
+                <td>Wind</td>
+                <td>{weather.list[0].wind.speed}</td> 
+              </tr>
+              <tr>
+                <td>Cloudiness</td>
+                <td>{weather.list[0].weather[0].main}</td> 
+              </tr>
+              <tr>
+                <td>Pressure</td>
+                <td>{weather.list[0].main.pressure}</td> 
+              </tr>
+              <tr>
+                <td>Humidity</td>
+                <td>{weather.list[0].main.humidity}</td> 
+              </tr>
+              <tr>
+                <td>Geo coords</td>
+                <td>{weather.city.coord.lat}</td> 
+                <td>{weather.city.coord.lon}</td> 
+              </tr>
             </tbody>
           </table>
       </div>
