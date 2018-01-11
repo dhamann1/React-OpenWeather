a2db9a8df480db4d1f97dfc18619010a250498b9
Dean Hamann <hamann.dean@gmail.com>
2018-01-10 23:59:39 -0800

    Build out WeatherGrid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,9 +30,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <WeatherGrid weather={this.state.weather} /> 
+        <WeatherGrid weather={this.state.weather}/> 
       </div>
     );
   }
--- src/components/WeatherGrid/WeatherGrid.jsx
+++ src/components/WeatherGrid/WeatherGrid.jsx
@@ -5,7 +5,26 @@ import './WeatherGrid.css';
 const WeatherGrid = ({weather}) => {
 	return (
     <div>
-			{!weather ? <p>Loading</p> : <p>{weather.city.name}</p> }
+      {!weather ? <p>Loading</p> :  
+      <div>
+          <h2> Weather in {weather.city.name}</h2>
+            <h3> {weather.list[0].main.temp} ° Degrees </h3> 
+            <table>
+              <tbody>
+                <tr>
+                  <td>Wind</td>
+                  <td>{weather.list[0].wind.speed}</td> 
+                </tr>
+                <tr></tr>
+                <tr></tr>
+                <tr></tr>
+                <tr></tr>
+                <tr></tr>
+            </tbody>
+          </table>
+      </div>
+        
+    }
     </div> 
   )
 }
